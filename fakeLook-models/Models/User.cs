608308977e39905ca214175954a4779a0422b7d0
Einat Sela <einat.sela2022@gmail.com~>
2022-03-14 11:52:08 +0200

    UserController - post

--- fakeLook-models/Models/User.cs
+++ fakeLook-models/Models/User.cs
@@ -9,9 +9,13 @@ namespace fakeLook_models.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+        public string UserName { get; set; }
+        public string WorkPlace { get; set; }
         public string Password { get; set; }
         public string Address { get; set; }
+        public int Age { get; set; }
 
         /* EF Relations */
         public virtual ICollection<Comment> Comments { get; set; }
