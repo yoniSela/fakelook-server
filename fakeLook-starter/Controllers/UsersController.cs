5d980222e75f882cc64078f905e8e623024e4dd5
Einat Sela <einat.sela2022@gmail.com~>
2022-03-14 11:44:58 +0200

    User repository and controller

---
+++ fakeLook-starter/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using fakeLook_models.Models;
+using fakeLook_starter.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace fakeLook_starter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+       
+        private readonly IUserRepository _repository;
+
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+
+        }
+
+        [HttpGet]
+        [Route("ById")]
+        [ResponseType(typeof(User))]
+        public JsonResult ById(int id)
+        {
+            return new JsonResult(_repository.GetById(id));
+        }
+
+        [HttpGet]
+        [Route("All")]
+        [ResponseType(typeof(ICollection<User>))]
+        public JsonResult All()
+        {
+            return new JsonResult(_repository.GetAll());
+        }
+
+       
+    }
+}
