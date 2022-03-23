e24943ba5d9d62bd7f3b2c28a5851dc121e8b53e
Einat Sela <einat.sela2022@gmail.com~>
2022-03-23 23:22:15 +0200

    catch token exception

--- fakeLook-starter/Controllers/UsersController.cs
+++ fakeLook-starter/Controllers/UsersController.cs
@@ -4,12 +4,15 @@ using fakeLook_starter.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Http.Description;
 
 namespace fakeLook_starter.Controllers
 {
-    [Route("api/[controller]")]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
     [ApiController]
     public class UsersController : Controller
     {
@@ -25,40 +28,40 @@ namespace fakeLook_starter.Controllers
 
         }
 
-        [HttpGet]
-        [Route("ById")]
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("ById")]
         [ResponseType(typeof(User))]
         public JsonResult ById(int id)
         {
             return new JsonResult(_repository.GetById(id));
         }
 
-        [HttpGet]
-        [Route("ByUsername")]
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("ByUsername")]
         [ResponseType(typeof(User))]
         public JsonResult ByName(string name)
         {
             return new JsonResult(_repository.GetByName(name));
         }
 
-        [HttpGet]
-        [Route("All")]
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("All")]
         [ResponseType(typeof(ICollection<User>))]
         public JsonResult All()
         {
             return new JsonResult(_repository.GetAll());
         }
 
-        [HttpPost]
-        [Route("SaveUser")]
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("SaveUser")]
         public void Add(User item)
         {
             _repository.Add(item);
         }
 
-        [HttpPost]
-        [Route("Login")]
-        public IActionResult Login([FromBody] User user)
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("Login")]
+        public IActionResult Login([Microsoft.AspNetCore.Mvc.FromBody] User user)
         {
             var dbUser = _repository.FindItem(user);
             if (dbUser == null) return Problem("user not in system");
@@ -67,44 +70,40 @@ namespace fakeLook_starter.Controllers
         }
 
 
-        [HttpPost]
-        [Route("SignUp")]
-        public IActionResult SignUp([FromBody] User user)
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [Microsoft.AspNetCore.Mvc.Route("SignUp")]
+        public IActionResult SignUp([Microsoft.AspNetCore.Mvc.FromBody] User user)
         {
             var dbUser = _repository.Post(user);
             var token = _tokenService.CreateToken(dbUser);
             return Ok(new { token });
         }
 
-        [HttpGet]
-        [Route("GetToken")]
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("GetToken")]
         public string SignUp(string token)
         {
-            return _tokenService.GetPayload(token);
+            try
+            {
+                return _tokenService.GetPayload(token);
+            } catch
+            {
+                return "Error";
+            }
         }
 
-        [HttpPut]
-        [Route("Edit")]
+        [Microsoft.AspNetCore.Mvc.HttpPut]
+        [Microsoft.AspNetCore.Mvc.Route("Edit")]
         public async Task<JsonResult> Edit(User user)
         {
             return new JsonResult(await _repository.Edit(user));
         }
-        [HttpGet]
-        [Route("GetDIct")]
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("GetDIct")]
         public async Task<JsonResult> GetDIct()
         {
             return new JsonResult(_repository.GetUserNames());
         }
-
-
-        //[HttpPut]
-        //[Route("ChangePassword")]
-        //public async Task<JsonResult> ChangePassword(string userName, string password)
-        //{
-        //    return new JsonResult(await _repository.ChangePassword(userName, password));
-        //}
-
-
 
     }
 }
